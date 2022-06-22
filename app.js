33fe79c2c94c709dd4147ebbba614933d4f596f0
_Sydni Miles_ <_[sydnibean14@gmail.com](mailto:sydnibean14@gmail.com)_>
2022-06-22 13:07:47 -0400

    Updates to APIs

--- app.js
+++ app.js
@@ -2,3 +2,31 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate"
+
+let form = document.querySelector("#searchForm");
+let searchInput = document.querySelector("#gifSearch");
+let img = document.querySelector("#img");
+let btn = document.querySelector("#btn"); 
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let searchInput = input.value;
+    fetch(`${GIPHY_URL}?api_key=${searchInput}`, {mode: "cors"}
+    )
+    .then((response) => {
+        return response.json();
+    }
+    ) 
+    .then((result) => {
+        console.log(result);
+        gifimg.src = result.data.images.original.url;
+        gifimg.alt = result.data.title;
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+    console.log(gifSearch);
+
+})
